edacf3866fedb3bfdb8f49d5cce231105d46f998
AugustinBunani <48664320+AugustinBunani@users.noreply.github.com>
2022-04-25 16:26:57 +1000

    1.0.1

---
+++ app/src/main/java/com/augustin/thestarclub/model/API.kt
@@ -0,0 +1,5 @@
+package com.augustin.thestarclub.model
+
+class API {
+    
+}
--- app/src/main/java/com/augustin/thestarclub/view/MainActivity.kt
+++ app/src/main/java/com/augustin/thestarclub/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.augustin.thestarclub
+package com.augustin.thestarclub.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.augustin.thestarclub.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
